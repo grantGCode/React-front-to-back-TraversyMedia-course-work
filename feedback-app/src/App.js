78fd97603c3b54d199b3c7135cf68d94e661cb61
Grant Bentley <gbentley0618@comcast.net>
2023-08-27 18:34:24 -0500

    feedback-app project module 3 intro to jsx done

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,5 +1,8 @@
-function App() {
-    return (<h1>Hello from the app component</h1>)
-}
+import React from 'react'
 
+function App() {
+    return React.createElement('div', { className: 'container' },
+    React.createElement('h1', {}, 'My App')
+    )
+}
 export default App
