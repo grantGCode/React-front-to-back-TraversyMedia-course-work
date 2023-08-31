67b6929c6e6069631d755420b041d465918e4355
Grant Bentley <gbentley0618@comcast.net>
2023-08-30 20:33:22 -0500

    Module 1 Components, Props & Props Types

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import Header from './components/Header'
 
 function App() {
-    return React.createElement('div', { className: 'container' },
-    React.createElement('h1', {}, 'My App')
+    return (
+    <>
+        <Header />
+        <div className='container'>
+          <h1>My App</h1>
+        </div>
+    </>
     )
 }
+
 export default App
