da1ab3b8923d2d32d4b7a1a0202b0d802a269e07
Grant Bentley <gbentley0618@comcast.net>
2023-09-02 03:19:02 -0500

    Module 4 Globale State-Feedback Item progress

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
+import FeedbackData from './data/feedbackData'
 function App() {
+  const [Feedback, setFeedback] = useState(FeedbackData)
+
     return (
     <>
         <Header />
