ea7a9d5ba577f29feef0320041d1a17d2ac52897
Grant Bentley <gbentley0618@comcast.net>
2023-09-02 03:08:58 -0500

    Module 3 State & useState Hook complete

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
+
 function FeedbackItem() {
-    return (
+  const [raiting, setRating] = useState(7)
+  const [text, setText] = useState('This is an example of a feedback item')  
+  
+  const handleClick = () => {
+    setRating((prev) => {
+      return prev + 1
+    })
+  }
+
+  return (
       <div className='card'>
-          <div className="num-display">10</div>
-          <div className="text-display">This is an example of a feedback item</div>
+          <div className="num-display">{raiting}</div>
+          <div className="text-display">{text}</div>
+          <button onClick={handleClick}>Click</button>
       </div>
     )
   }
