a1a67ddbca92904e008b7bdad966e70025761e23
Grant Bentley <gbentley0618@comcast.net>
2023-09-02 15:07:21 -0500

    Module 4 Global State - Feedback Item done

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,13 +3,13 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/feedbackData'
 function App() {
-  const [Feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData)
 
     return (
     <>
         <Header />
         <div className='container'>
-          <FeedbackList feedback={Feedback} />
+          <FeedbackList feedback={feedback} />
         </div>
     </>
     )
