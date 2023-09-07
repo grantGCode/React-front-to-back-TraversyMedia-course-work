97b48fd44a23b9a94e87bec531dff7e62364c5f4
Grant Bentley <gbentley0618@comcast.net>
2023-09-06 22:31:14 -0500

    Module 3 Real-Time Validation progress

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import Card from './shared/card'
+import Button from './shared/Button'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handTextChange = (e) => {
-        setText(e.target.value)
+      if (text === '') {
+        setBtnDisabled (true)
+        setMessage(NaN)
+      }else if (text !== '' && text.trim().length <= 10) {
+        setBtnDisabled (true)
+        setMessage('Text must be at least 10 characters')
+      } else {
+        setMessage(null)
+        setBtnDisabled(false)
+      }
+
+
+      setText(e.target.value)
     }
 
   return <Card>
@@ -14,9 +29,10 @@ function FeedbackForm() {
         {/* @todo - rating select component*/}
         <div className="input-group">
             <input onChange={handTextChange} type='text' placeholder='Wright a review' value={text} />
-            <button  type='submit'>Send</button>
+            <Button  type='submit' isDisabled={btnDisabled}>Send</Button>
             {/* @todo - button will be in it's own component this is a place holder for now*/}
         </div>
+        {message && <div className='message'>{message}</div>}
     </form>
 
     </Card>
