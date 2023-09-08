e2a2c8f2b343aa3d38c41c02c431728f17da3c54
Grant Bentley <gbentley0618@comcast.net>
2023-09-07 21:25:23 -0500

    Module 4 completed Raiting Selection Component

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import RaitingSelect from './RaitingSelect'
 import Card from './shared/card'
 import Button from './shared/Button'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRaiting] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
     const handTextChange = (e) => {
       if (text === '') {
         setBtnDisabled (true)
-        setMessage(NaN)
+        setMessage('')
       }else if (text !== '' && text.trim().length <= 10) {
         setBtnDisabled (true)
         setMessage('Text must be at least 10 characters')
@@ -27,12 +29,12 @@ function FeedbackForm() {
     <form>
         <h2>Houw would you rate our service?</h2>
         {/* @todo - rating select component*/}
+        <RaitingSelect select={(rating) => setRaiting(rating)}/>
         <div className="input-group">
             <input onChange={handTextChange} type='text' placeholder='Wright a review' value={text} />
             <Button  type='submit'isDisabled={btnDisabled}>Send</Button>
-            {/* @todo - button will be in it's own component this is a place holder for now*/}
+
         </div>
-        {message && <div className='message'>{message}</div>}
     </form>
 
     </Card>
