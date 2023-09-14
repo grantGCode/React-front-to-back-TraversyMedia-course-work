ee4076ec9658fc5c634e094c67f62d332ed44635
Grant Bentley <gbentley0618@comcast.net>
2023-09-13 22:36:17 -0500

    Module 3 "Custom Links" complete

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from './data/feedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
             </Route>
           <Route path= '/about' element={<AboutPage />} />
         </Routes>
+        <AboutIconLink />
       </div>
     </Router>
     )
--- feedback-app/src/pages/AboutPage.jsx
+++ feedback-app/src/pages/AboutPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Card from '../components/shared/card'
+import {Link} from 'react-router-dom'
 
 function AboutPage() {
   return (
     <Card>
       <div className='about'>
-          <p>This is a React app leave feedback for a product or service.</p>
+          <h1>About This Project</h1>
+          <p>This is a React app to leave feedback for a product or service.</p>
           <p>Version: 1.0.0</p>
 
           <p>
-            <a herf='/'>Back To Home</a>
+            <Link to='/'>Back To Home</Link>
           </p>
       </div>
     </Card>
