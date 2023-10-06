050a27d195a1b61bf0434bbbdf082fb79c9d7059
Grant Bentley <gbentley0618@comcast.net>
2023-10-05 22:06:21 -0500

    Module 4 "Display User" Complete

--- github-finder/src/commponents/users/UserResults.jsx
+++ github-finder/src/commponents/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {useEffect} from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from '../users/UserItem'
 
 function UserResults() {
 const [users, setUsers] = useState([])
@@ -27,7 +28,7 @@ if(! loading) {
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {users.map((user) => 
-      (<h3>{user.login}</h3>))}
+      (<UserItem key={user.id} user={user}/>))}
       </div> 
     )
       } else {
