3673e6ca05c818f2d4f0223e01ba2de9f4b6fc4f
Grant Bentley <gbentley0618@comcast.net>
2023-10-14 18:52:39 -0500

    change "Top Repositories" to "Latest Repositories" in RepoList.jsx

--- github-finder/src/commponents/Repos/RepoList.jsx
+++ github-finder/src/commponents/Repos/RepoList.jsx
@@ -7,7 +7,7 @@ function RepoList({repos}) {
     <div className='rounded-lg shadow-lg card bg-base-100'>
       <div className='card-body'>
         <h2 className='text-3xl my-4 font-bold card-title'>
-          Top Repositories
+          Latest Repositories
         </h2>
         {repos.map((repo) => (<h3>{repo.name}</h3>))}
       </div>
