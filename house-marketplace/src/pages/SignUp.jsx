1e7f99d3fce0bb7abd8968b10b5651bd73bb560d
Grant Bentley <gbentley0618@comcast.net>
2023-11-07 22:27:50 -0600

    module 2 progress 99% done error with Node_modules and firebase

--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config.js'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -14,11 +16,34 @@ function SignUp() {
 
   const navigate = useNavigate()
 
+  
+  
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }))
+  }
+  
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -29,7 +54,7 @@ function SignUp() {
        </header>
 
        <main>
-        <form>
+        <form onSubmit={onSubmit}>
         <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange} />
           <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
           <div className='passwordInputDiv'>
