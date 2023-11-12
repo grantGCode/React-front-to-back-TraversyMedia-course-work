8d3591642d8ef97abd049e8b1eaa2fcf0249b0a8
Grant Bentley <gbentley0618@comcast.net>
2023-11-11 18:00:17 -0600

    Module 6 "User Logout" compleyte

--- house-marketplace/src/pages/profile.jsx
+++ house-marketplace/src/pages/profile.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { getAuth} from 'firebase/auth'
+import { useNavigate, Link } from 'react-router-dom'
 
 function Profile() {
-  const [user, setUser] = useState(null)
-
   const auth = getAuth()
-  useEffect(() => {
-    setUser(auth.currentUser);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-  }, [])
+  const {name, email } = formData
 
-  return user ? <h1>{user.displayName}</h1> : <h1>Not Logged In</h1>
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  // return user ? <h1>{user.displayName}</h1> : <h1>Not Logged In</h1>
 
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  )
-}
+  <div className='profile'> 
+    <header className='profileHeader'>
+      <p className="pageHeader">My Profile</p>
+      <button type='button' className='LogOut' onClick={onLogout}>Logout</button>
+    </header>
+  </div>
+  )}
 
 export default Profile
