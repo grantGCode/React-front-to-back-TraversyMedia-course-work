4e05457e218df7d555caffe9c7d12bce5ca2c910
Grant Bentley <gbentley0618@comcast.net>
2023-11-14 21:44:38 -0600

    Module 3 "Listing Item Component" complete

--- house-marketplace/src/components/ListingItem.jsx
+++ house-marketplace/src/components/ListingItem.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {ReactComponet as DeleteIcon} from '../assets/svg/deleteIcon.svg'
+import {ReactComponent as DeleteIcon} from '../assets/svg/deleteIcon.svg'
 import BathtubIcon from '../assets/svg/bathtubIcon.svg'
 import bedIcon from '../assets/svg/bedIcon.svg'
 
 
-function ListingItem({listing, id }) {
+function ListingItem({listing, id, onDelete }) {
   return (
     <div>
         <li className='categoryListing'>
-            <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'></Link>
-            <img src={listing.imageUrls[1]} alt={listing.name} className='categoryListingImg' />
+            <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
+              <img src={listing.imageUrls[1]} alt={listing.name} className='categoryListingImg' />
+              <div className="categoryListingDetails">
+                <p className="categoryListingLocation">{listing.location}</p>
+                <p className="categoryListingName">{listing.name}</p>
+
+                <p className="categoryListingPrice">${listing.offer ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  : listing.regularPrice}</p>
+                  {listing.type === 'rent' && '/ Month'}
+                  <div className="categoryListingDiv">
+                    <img src={bedIcon} alt="Bed"/>
+                    <p className='categoryListingInfoText'>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}</p>
+                    <img src={BathtubIcon} alt='Bath' />
+                    <p className='categoryListingInfoText'>{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}</p>
+                  </div>
+              </div>
+            </Link>
+
+            {onDelete && (
+              <DeleteIcon className='removeIcon' fill='rgb(231, 76, 60)'  onClick={() => onDelete(listing.id, listing.name)} />
+            )}
         </li>
     </div>
   )
