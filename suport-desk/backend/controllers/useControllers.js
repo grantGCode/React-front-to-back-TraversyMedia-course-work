c602f24480c609a88187a8c0f389a6a82de033b5
Grant Bentley <gbentley0618@comcast.net>
2023-11-25 01:40:06 -0600

    Module 6 "Add Routes Controller" complete

---
+++ suport-desk/backend/controllers/useControllers.js
@@ -0,0 +1,14 @@
+
+const {} = require('')
+
+const registerUser = (req, res) => {
+    res.semd('Register Route')
+}
+
+const loiginUser = (req, res) => {
+    res.send('Login Route')
+}
+
+
+module.export = router
+
--- suport-desk/backend/routes/userRoutes.js
+++ suport-desk/backend/routes/userRoutes.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const {registerUser, loginUser} = require('../controllers/useControllers')
 
-router.post('/', (req, res) => {
-    res.send('Register Route')
-})
-
-router.post('/login', (req, res) => {
-    res.send('Login Route')
-})
+router.post('/', registerUser)
+router.post('/login', loginUser)
 
 
 module.exports = router
