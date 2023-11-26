55efc24def4f0d85377fc6b6f7570340be9aff3f
Grant Bentley <gbentley0618@comcast.net>
2023-11-25 20:54:11 -0600

    Module 7 progress: add user validation via postman

--- suport-desk/backend/controllers/useControllers.js
+++ suport-desk/backend/controllers/useControllers.js
@@ -1,8 +1,19 @@
-
+// @desc    Register a new user
+// @route   /api/users
+//@access   Public
 const registerUser = (req, res) => {
-    res.semd('Register Route')
+   const {name, email, password} =req.body
+
+   //Vallidation
+    if (!name || !email || !password) {
+        res.status(400).json({message: 'Please inculde all fields.'})
+    }
+    res.send('Register Route')
 }
 
+// @desc    Login a user
+// @route   /api/users
+//@access   Public
 const loginUser = (req, res) => {
     res.send('Login Route')
 }
