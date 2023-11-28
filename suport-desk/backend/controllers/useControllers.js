4c2b0da511557529336b38cc759321bea7d27a9c
Grant Bentley <gbentley0618@comcast.net>
2023-11-27 23:20:34 -0600

    Module 10 Progress: Check if passwords match

--- suport-desk/backend/controllers/useControllers.js
+++ suport-desk/backend/controllers/useControllers.js
@@ -52,7 +52,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/users
 //@access   Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+    const {email, password} = req.blody
+
+    const user =await User.findOne({email})
+    //Check user and passwords match
+    if(user && (await bcrypt.compare(password, user.password))){
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            })
+            }else{
+                res.status(401)
+                throw new Error('Invalid credentials')
+            }
+
+  
 })
 
 
