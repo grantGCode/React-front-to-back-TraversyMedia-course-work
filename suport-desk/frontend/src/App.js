d701ca2baceed0f4b7bec3b6479c6e17042707c3
Grant Bentley <gbentley0618@comcast.net>
2023-12-02 23:48:38 -0600

    Module 2 "Header Initial Pages" Complete

--- suport-desk/frontend/src/App.js
+++ suport-desk/frontend/src/App.js
@@ -1,7 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Headers';
+import Home from'./pages/Home'
+import Login from'./pages/Login'
+import Register from './pages/Register';
 
 function App() {
   return (
-   <div>My App</div>
+   <>
+   <Router>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
+        </Routes>
+      </div>
+   </Router>
+   </>
   );
 }
 
