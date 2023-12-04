2420361979ac8e6919795758d660fd66b9370889
Grant Bentley <gbentley0618@comcast.net>
2023-12-03 23:55:39 -0600

    saved progress

--- suport-desk/backend/controllers/useControllers.js
+++ suport-desk/backend/controllers/useControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler =require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User =  require('../models/userModel')
 
 // @desc    Register a new user
 // @route   /api/users
@@ -11,6 +14,37 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
        throw new Error('Please include all fields')
     }
+
+    //Find if user already exists
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Creat user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id, //Id has "_" becasues thisis how databaes stores it's id's
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
     res.send('Register Route')
 })
 
@@ -18,7 +52,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/users
 //@access   Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+    const {email, password} = req.blody
+
+    const user =await User.findOne({email})
+    //Check user and passwords match
+    if(user && (await bcrypt.compare(password, user.password))){
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            })
+            }else{
+                res.status(401)
+                throw new Error('Invalid credentials')
+            }
+
+
 })
 
 
